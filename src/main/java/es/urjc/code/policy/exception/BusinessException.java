62b0e24f2b87460b055297f47ad1ea4b151c9697
François <fpoirier2002@gmail.com>
2020-09-23 21:54:15 +0200

    error handler

---
+++ src/main/java/es/urjc/code/policy/exception/BusinessException.java
@@ -0,0 +1,42 @@
+package es.urjc.code.policy.exception;
+
+public class BusinessException extends RuntimeException {
+
+	protected static final Object[] EMPTY_ARGS = new Object[0];
+
+    private String code = null;
+    private Object[] args = EMPTY_ARGS;
+
+    
+    public String getCode() {
+		return code;
+	}
+
+	public Object[] getArgs() {
+		return args;
+	}
+
+	public BusinessException(String code) {
+        super(code);
+        this.code = code;
+        this.args = EMPTY_ARGS;
+    }
+
+    public BusinessException(String code, String message) {
+        super(message);
+        this.code = code;
+        this.args = EMPTY_ARGS;
+    }
+
+    public BusinessException(String code, String message, Object ... args) {
+        super(message);
+        this.code = code;
+        this.args = args;
+    }
+
+    public BusinessException(String code, Object ... args) {
+        super(code);
+        this.code = code;
+        this.args = args;
+    }
+}
